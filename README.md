a0fb6f45a0313105c2855c783a79aebbfeec2250
skremko <50918105+skremko@users.noreply.github.com>
2019-05-23 01:58:11 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-# pytest_tests_execution_flow
+
+
+pytest-tests-execution-flow
+==============================================================================
+
+:Authors: Sergii Kremko
+:Version: 0.0.1
+:Code: https://github.com/skremko/pytest_tests_execution_flow
+
+
+pytest-tests-execution-flow is a plugin for pytest, which allows to set tests that
+must be started at the beginning and end of the session. The plugin also provides two marks
+`skip_all_after_this_fail` and `fail_all_after_this_fail`, which allows you to skip\fail
+next tests if the marked tests fail.
+
+Requirements
+==============================================================================
+
+You will need the following prerequisites in order to use pytest-tests-execution-flow:
+
+- Python 2.7, 3.6
+- Pytest 4.1.0 or higher
+
+Installation
+==============================================================================
+Install the plugin with::
+
+    pip install pytest-tests-execution-flow
+
+Usage
+==============================================================================
+Prepare the config file :code:`pytest.ini` in root directory of tests
+
+Example of :code:`pytest.ini`:
+
+.. code-block:: text
+
+    [pytest]
+    run_at_start=
+        test_5
+        test_4 
+    run_at_end=
+        test_1
+        test_2 
+
+If same test name exist at both parameters it will be run at start.
---
+++ pytest_test_execution_flow.py
@@ -0,0 +1,78 @@
+import pytest
+
+
+MARKS = {
+    'skip_all_after_this_fail': pytest.skip,
+    'fail_all_after_this_fail': pytest.fail,
+}
+
+
+def pytest_addoption(parser):
+    parser.addini(
+        'run_at_start',
+        type='args',
+        help='These tests will be launched at the beginning of the session.'
+    )
+
+    parser.addini(
+        'run_at_end',
+        type='args',
+        help='These tests will be launched at the ending of the session.'
+    )
+
+
+def pytest_configure(config):
+    for mark in MARKS:
+        config.addinivalue_line('markers', mark)
+    run_at_start = config.getini('run_at_start')
+    run_at_end = config.getini('run_at_end')
+    config.sequence_manager = SequenceManager(run_at_start, run_at_end)
+    config.pluginmanager.register(config.sequence_manager)
+
+
+def pytest_unconfigure(config):
+    sequence_manager = getattr(config, 'sequence_manager', None)
+    if sequence_manager:
+        config.pluginmanager.unregister(config.sequence_manager)
+        del config.sequence_manager
+
+
+class SequenceManager:
+    def __init__(self, run_at_start, run_at_end):
+        self.action = None
+        self.reason = None
+        self.run_at_start = run_at_start
+        self.run_at_end = [i for i in run_at_end if i not in run_at_start]
+
+    def pytest_collection_modifyitems(self, session, config, items):
+        _items = items[:]
+        nose, tail = [], []
+        for test_name in self.run_at_start:
+            for k in _items:
+                if k.name == test_name:
+                    _items.remove(k)
+                    nose.append(k)
+        for test_name in self.run_at_end:
+            for k in _items:
+                if k.name == test_name:
+                    _items.remove(k)
+                    tail.append(k)
+        items[:] = nose + _items + tail
+
+    @pytest.hookimpl(tryfirst=True, hookwrapper=True)
+    def pytest_runtest_makereport(self, item, call):
+        outcome = yield
+        res = outcome.get_result()
+        if res.when == 'call':
+            if res.outcome == 'failed' and not self.action:
+                for mark, action in MARKS.items():
+                    if item.get_closest_marker(mark):
+                        self.action = action
+                        self.reason = action.__name__
+                        break
+
+    @pytest.hookimpl(tryfirst=True, hookwrapper=True)
+    def pytest_runtest_setup(self, item):
+        if self.action:
+            msg = 'This test {0}ed because test - {1} was failed!'.format(self.reason, item.name)
+            self.action(msg)
