c5b4d0d5db016481d9d112844e6093ec50b28c1c
skremko <50918105+skremko@users.noreply.github.com>
2019-05-24 17:20:17 +0300

    Update pytest_test_execution_flow.py

--- pytest_test_execution_flow.py
+++ pytest_test_execution_flow.py
@@ -71,7 +71,6 @@ class SequenceManager:
                         self.reason = action.__name__
                         break
 
-    @pytest.hookimpl(tryfirst=True, hookwrapper=True)
     def pytest_runtest_call(self, item):
         if self.action:
             msg = 'This test {0}ed because test - {1} was failed!'.format(self.reason, item.name)
