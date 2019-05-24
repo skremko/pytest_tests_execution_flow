32756fbe7e706f9b4d29bc8285fcd73af9070c99
skremko <50918105+skremko@users.noreply.github.com>
2019-05-24 16:46:59 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ with open("README.md", "r") as fh:
 setuptools.setup(
      name='pytest-tests-execution-flow',
      version='0.0.1',
-     script=['pytest_tests_execution_flow'],
      author="Sergii Kremko",
      author_email="sergii.kremko@gmail.com",
      description="Plugin for pytest",
      long_description=long_description,
-     long_description_content_type="text/markdown",
-     url="https://github.com/javatechy/dokr",
+     long_description_content_type='text/markdown',
+     url='https://github.com/pytest-dev/pytest-twisted',
+     py_modules=['pytest_tests_execution_flow'],
      entry_points={'pytest11': ['pytest_test_execution_flow = pytest_test_execution_flow']},
      install_requires=[
        'pytest>=4.1.0'
