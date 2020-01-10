bfd4e527b1830e00e3a43b6536959e04622226f2
skremko <50918105+skremko@users.noreply.github.com>
2020-01-10 19:36:14 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
      name='pytest-tests-execution-flow',
-     version='0.0.1',
+     version='0.0.2',
      author="Sergii Kremko",
      author_email="sergii.kremko@gmail.com",
      description="Plugin for pytest",
